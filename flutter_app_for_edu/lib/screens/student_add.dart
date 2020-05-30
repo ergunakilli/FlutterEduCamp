5d4f02714cd1819210ccc6af52eb1e301afe5659
xxzenon <ergunakilli@gmail.com>
2020-05-31 02:58:10 +0300

    last commit

--- flutter_app_for_edu/lib/screens/student_add.dart
+++ flutter_app_for_edu/lib/screens/student_add.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutterappforedu/Modals/student.dart';
+import 'package:flutterappforedu/models/student.dart';
 
 class StudentAdd extends StatefulWidget {
 
